9c9ec40b601d6c11193f6533522ba349ad6eb1fe
KPLclearlove <sixgod1240@foxmail.com>
2024-04-13 00:03:21 +0800

    Initial commit

---
+++ Week7/Matrix.java
@@ -0,0 +1,80 @@
+package Week7;
+
+public class Matrix {
+    private double[][] values;
+    private int nrows, ncols;
+
+    public Matrix(int nrows, int ncols) {
+        this.nrows = nrows;
+        this.ncols = ncols;
+        this.values = new double[nrows][ncols];
+    }
+
+    public Matrix(double[][] values) {
+        this.nrows = values.length;
+        this.ncols = values[0].length;
+        this.values = values;
+    }
+
+    public Matrix add(Matrix mat) {
+        if (nrows != mat.nrows || ncols != mat.ncols) {
+            return null;
+        }
+
+        double[][] newValues = new double[nrows][ncols];
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                newValues[i][j] = values[i][j] + mat.values[i][j];
+            }
+        }
+
+        return (new Matrix(newValues));
+    }
+
+    public Matrix substract(Matrix mat) {
+        if (nrows != mat.nrows || ncols != mat.ncols) {
+            return null;
+        }
+
+        double[][] newValues = new double[nrows][ncols];
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                newValues[i][j] = values[i][j] - mat.values[i][j];
+            }
+        }
+
+        return (new Matrix(newValues));
+    }
+
+    public Matrix multiplicate(Matrix mat) {
+        if (ncols != mat.nrows) {
+            return null;
+        }
+
+        double[][] newValues = new double[nrows][mat.ncols];
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < mat.ncols; j++) {
+                double sum = 0;
+                for (int k = 0; k < ncols; k++) {
+                    sum += values[i][k] * mat.values[k][j];
+                }
+                newValues[i][j] = sum;
+            }
+        }
+
+        return (new Matrix(newValues));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                sb.append("\t" + values[i][j]);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
---
+++ Week7/MatrixTester.java
@@ -0,0 +1,26 @@
+package Week7;
+
+public class MatrixTester {
+    public static void main(String[] args) {
+        double[][] values1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        double[][] values2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+
+        Matrix mat1 = new Matrix(values1);
+        Matrix mat2 = new Matrix(values2);
+
+        System.out.println("matrix1: ");
+        System.out.println(mat1);
+
+        System.out.println("matrix2: ");
+        System.out.println(mat2);
+
+        System.out.println("matrix1 + matrix2: ");
+        System.out.println(mat1.add(mat2));
+
+        System.out.println("matrix1 - matrix2: ");
+        System.out.println(mat1.substract(mat2));
+
+        System.out.println("matrix1 * matrix2: ");
+        System.out.println(mat1.multiplicate(mat2));
+    }
+}
